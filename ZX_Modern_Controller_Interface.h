289da9e284a2c62209b511e22d17cb2395719880
Andrej Luchava <andrej.luchava@mediaconstrux.com>
2017-09-11 10:00:48 +0200

    test

--- ZX_Modern_Controller_Interface.h
+++ ZX_Modern_Controller_Interface.h
@@ -1,14 +1,15 @@
 #ifndef _ZX_Modern_Controller_Interface_H_
 #define _ZX_Modern_Controller_Interface_H_
 
-#include "Arduino.h"
+#include <Arduino.h>
+#include <usbhub.h>
+#include <hidboot.h>
+#include <hiduniversal.h>
+
 #include "KeyboardReportParserImpl.h"
 #include "JoystickReportParserImpl.h"
 #include "JoystickConnector.h"
 #include "KeyboardMapping.h"
-#include <hidboot.h>
-#include <hiduniversal.h>
-#include <usbhub.h>
 #include "LogitechDualAction.h"
 
 #endif /* _ZX_Modern_Controller_Interface_H_ */
